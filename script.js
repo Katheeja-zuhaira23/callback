575b078100e77a3714d96e81e7c654df3adb7af8
Katheeja-zuhaira23 <katheejakamal20@gmail.com>
2023-10-29 13:34:21 +0530

    callback

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-setTimeout(function () {
+setTimeout(()=> {
     document.getElementById("countdown").innerHTML = "10";
-    setTimeout(function () {
+    setTimeout(()=>{
       document.getElementById("countdown").innerHTML = "9";
-      setTimeout(function () {
+      setTimeout(()=>{
         document.getElementById("countdown").innerHTML = "8";
-        setTimeout(function () {
+        setTimeout(()=>{
           document.getElementById("countdown").innerHTML = "7";
-          setTimeout(function () {
+          setTimeout(()=>{
             document.getElementById("countdown").innerHTML = "6";
-            setTimeout(function () {
+            setTimeout(()=>{
               document.getElementById("countdown").innerHTML = "5";
-              setTimeout(function () {
+              setTimeout(()=>{
                 document.getElementById("countdown").innerHTML = "4";
-                setTimeout(function () {
+                setTimeout(()=>{
                   document.getElementById("countdown").innerHTML = "3";
-                  setTimeout(function () {
+                  setTimeout(()=>{
                     document.getElementById("countdown").innerHTML = "2";
-                    setTimeout(function () {
+                    setTimeout(()=> {
                       document.getElementById("countdown").innerHTML = "1";
-                      setTimeout(function () {
+                      setTimeout(()=> {
                         document.getElementById("countdown").innerHTML = "HAPPY INDEPENDENCE DAY!!";
                       }, 1000);
                     }, 1000);
